116a209594170d1db4072036935b195de0cf5b89
Kanono Ramashamole <kanono@gmail.com>
2023-01-10 12:57:14 +0200

    Completed route protection

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 // Internal imports
 import { loginUser, registerService, registerUser } from '../controllers/authControllers.js';
+import { verifyServiceProvider } from '../utils/verifyToken.js';
 
 const router = express.Router()
 
-router.post("/register-user", registerUser)
-router.post("/register-service", registerService)
+router.post("/register-user",registerUser)
+router.post("/register-service", verifyServiceProvider, registerService)
 router.post('/login', loginUser)
 
 export default router
--- server/src/routes/usersRoutes.js
+++ server/src/routes/usersRoutes.js
@@ -6,21 +6,21 @@ import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 const router = express.Router();
 
 
-router.put("/:id", updateUser)
-router.delete("/:id", deleteUser)
-router.get("/checkauthentication",verifyToken, (req,res,next)=>{
+router.put("/:id", verifyUser, updateUser)
+router.delete("/:id", verifyUser,deleteUser)
+
+router.get("/get-users",verifyAdmin, getUsers)
+router.get("/:id",verifyUser, getUserById)
+
+/*router.get("/checkauthentication",verifyToken, (req,res,next)=>{
     res.send("hello user, you are logged in")
   })
-router.get("/get-users", getUsers)
-router.get("/:id", getUserById)
-
-
 router.get("/checkuser/:id", verifyUser,(req,res,next)=>{
    res.send("hello user, you are logged in and you can delete your account")
 })
 
 router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
    res.send("hello admin, you are logged in and you can delete all accounts")
-})
+})*/
 
 export default router;
