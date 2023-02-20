bab683c86963ef906d954eb4377a599370510fdf
Kanono Ramashamole <kanono@gmail.com>
2023-02-20 18:54:11 +0200

    Build navbar

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'import/extensions': 0,
     'react/prop-types': 0,
+    'no-unused-vars': 'warn',
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
